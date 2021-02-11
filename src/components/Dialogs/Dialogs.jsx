badc87b62a1e7bf3be271154f42978870909abce
Evgeny-Chabrovsky <chabor111@gmail.com>
2021-02-12 02:01:16 +0300

    Exercise 22 Route exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./Dialogs.module.css";
 
 const Dialogs = () => {
   return (
     <div className={style.dialogs}>
       <div className={style.dialogsItems}>
-        <div className={style.dialog + " " + style.active}>Dimych</div>
-        <div className={style.dialog}>Dimych</div>
-        <div className={style.dialog}>Dimych</div>
-        <div className={style.dialog}>Dimych</div>
-        <div className={style.dialog}>Dimych</div>
-        <div className={style.dialog}>Dimych</div>
+        <div className={style.dialog + " " + style.active}>
+          <NavLink to="/dialogs/1">Dimych</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/2">Andrey</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/3">Sveta</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/4">Sasha</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/5">Viktor</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/6">Valera</NavLink>
+        </div>
       </div>
       <div className={style.messages}>
         <div className={style.message}>Hi!</div>
